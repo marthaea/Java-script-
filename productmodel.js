e87d5654ef4dd0bf08a9a8d2007421e8c3b962c5
marthapraisekatusiime <marthapraisekatusiime@gmail.com>
2024-11-20 15:19:57 +0300

    Add files via upload

---
+++ productmodel.js
@@ -0,0 +1,12 @@
+
+class Product {
+    constructor(productName, price, stock, category) {
+      this.productName = productName;
+      this.price = price;
+      this.stock = stock;
+      this.category = category;
+    }
+  }
+  
+  module.exports = Product;
+  
